866a4654939dc139070f8ba084b0b0441ef5b6ba
kaytise <84756104+kaytise@users.noreply.github.com>
2022-04-15 17:18:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
